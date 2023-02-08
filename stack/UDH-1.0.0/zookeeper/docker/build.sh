0e6531e4bb3d8760335c3df7f77eb43549e813ef
Pandas886 <huzk8@hotmail.com>
2023-02-08 10:07:30 +0000

    增加zk容器镜像

---
+++ stack/UDH-1.0.0/zookeeper/docker/build.sh
@@ -0,0 +1,3 @@
+docker build -f Dockerfile -t zookeeper:3.5.9 .
+docker tag zookeeper:3.5.9 registry.cn-hangzhou.aliyuncs.com/udh/zookeeper:3.5.9
+docker push registry.cn-hangzhou.aliyuncs.com/udh/zookeeper:3.5.9
