7128f7faedaa54f120ab0c60b3201e83f44b68e7
pandas886 <123344357+pandas886@users.noreply.github.com>
2023-02-21 18:39:45 +0800

    增加doris安装包

---
+++ stack/UDH-1.0.0/doris/docker/gcutil.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+export PATH=/sbin:/bin:/usr/sbin:/usr/bin:/usr/local/sbin:/usr/local/bin
+
+basedir=$(cd `dirname $0`/..; pwd)
+
+checkUser() {
+  if [ "`whoami`" == "root" ]; then
+   echo "The root user is not allowed to run."
+   exit 1
+  fi
+}
+checkUser
+
+jstat=${JAVA_HOME}/bin/jstat
+
+app_name=doris_fe
+app_pid=$(ps aux |grep java |grep  org.apache.doris.PaloFe |awk '{print $2}')
+
+echo "app_pid == $app_pid"
+$jstat -gcutil $app_pid 1000
