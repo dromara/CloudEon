febeb823da04c694613ee1409faf0dcc39dfb865
pandas886 <123344357+pandas886@users.noreply.github.com>
2023-03-02 16:10:45 +0800

    实现保存服务实例配置

---
+++ powerjob-server/powerjob-server-starter/src/main/java/com/data/udh/dto/ServicePresetConf.java
@@ -0,0 +1,12 @@
+package com.data.udh.dto;
+
+import lombok.Data;
+
+@Data
+public  class ServicePresetConf {
+
+    private String name;
+    private String value;
+    private String recommendedValue;
+
+}
