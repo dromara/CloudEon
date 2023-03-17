80f75f95215313e84cd19e3059092349ab4a9f53
pandas886 <huzk8@hotmail.com>
2023-03-17 16:11:25 +0800

    实现查询正在运行的指令数

--- cloudeon-server/src/main/java/com/data/udh/dao/CommandRepository.java
+++ cloudeon-server/src/main/java/com/data/udh/dao/CommandRepository.java
@@ -1,10 +1,13 @@
 package com.data.udh.dao;
 
 import com.data.udh.entity.CommandEntity;
+import com.data.udh.utils.CommandState;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
 public interface CommandRepository extends JpaRepository<CommandEntity, Integer> {
     List<CommandEntity> findByClusterIdOrderBySubmitTimeDesc(Integer clusterId);
+
+    long countByCommandStateAndClusterId(CommandState commandState,Integer clusterId);
 }
