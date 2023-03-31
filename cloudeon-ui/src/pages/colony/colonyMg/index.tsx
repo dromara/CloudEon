396958061bf6eee19cdeb862e64b6909bb76debe
Mericol <2472482459@qq.com>
2023-03-31 17:19:50 +0800

    view: 集群显示服务数和节点数，新增集群加kubeConfig参数，告警分tab

--- cloudeon-ui/src/pages/colony/colonyMg/index.tsx
+++ cloudeon-ui/src/pages/colony/colonyMg/index.tsx
@@ -3,9 +3,15 @@ import { PageContainer, ProCard } from '@ant-design/pro-components';
 import { SlackOutlined, DeleteOutlined, EditOutlined, EllipsisOutlined } from '@ant-design/icons';
 import { Space, Popconfirm, Button, Modal, Form, Input, message, Spin, Select } from 'antd';
 import { FormattedMessage, useIntl, history, useModel } from 'umi';
-import { useState, useEffect } from 'react';
+import { useState, useEffect, SetStateAction } from 'react';
 import styles from './index.less';
 import { getClusterListAPI, getStackListAPI, createClusterAPI } from '@/services/ant-design-pro/colony';
+
+import AceEditor from "react-ace";
+import "ace-builds/src-noconflict/mode-java";
+import "ace-builds/src-noconflict/theme-xcode";
+import "ace-builds/src-noconflict/ext-language_tools";
+
 
 const { Option } = Select;
 
@@ -17,6 +23,7 @@ const Colony: React.FC = () => {
   const [stackLoading, setStackLoading] = useState(false);
   const [clusterList, setClusterList] = useState<API.ColonyItem[]>();
   const [stackList, setStackList] = useState<API.StackItem[]>();
+  const [kubeConfig, setKubeConfig] = useState('');
   const [colonyForm] = Form.useForm();
 
   const getClusterData = async (params: any) => {
@@ -46,6 +53,7 @@ const Colony: React.FC = () => {
     colonyForm
       .validateFields()
       .then(async (values) => {
+        // console.log('values',values);
         const result: API.normalResult = await createClusterAPI(values)
         if(result && result.success){
           message.success('新增成功');
@@ -92,6 +100,11 @@ const Colony: React.FC = () => {
   const onSelectChange = (value: string) => {
     console.log('onSelectChange:', value);
   };
+
+  const onEditorChange = (value: any) => {
+    console.log(value);
+    
+  }
   
   // const { colonyData, setClusterId } = useModel('colonyModel', model => ({ colonyData: model.colonyData, setClusterId: model.setClusterId }));
 
@@ -155,9 +168,15 @@ const Colony: React.FC = () => {
                   history.push('/colony/nodeList?clusterId='+cItem.id);
                 }}
               >
-                <div className={styles.cardIcon}>
-                  <SlackOutlined key="slack1" />
-                  <div className={styles.cardDescText}>{cItem.clusterName}</div>
+                <div className={styles.stackWrap}>
+                  <div className={styles.titleWrap}>
+                    <SlackOutlined className={styles.cardIcon} />
+                    <div className={styles.stackTitle}>{cItem.clusterName}</div>
+                  </div>
+                  <div className={styles.stackInfo}>
+                    <div className={styles.stackDesc}>服务数：{cItem.serviceCnt}</div>
+                    <div className={styles.stackDesc}>节点数：{cItem.nodeCnt}</div>
+                  </div>
                 </div>
               </ProCard>
           )
@@ -171,6 +190,7 @@ const Colony: React.FC = () => {
         destroyOnClose={false}
         open={isModalOpen}
         onOk={handleOk}
+        width={800}
         onCancel={handleCancel}
         // footer={null}
       >
@@ -191,7 +211,7 @@ const Colony: React.FC = () => {
               name="clusterName"
               rules={[{ required: true, message: '请输入集群名称!' }]}
             >
-              <Input />
+              <Input className={styles.inputWrap}/>
             </Form.Item>
 
             <Form.Item
@@ -203,6 +223,7 @@ const Colony: React.FC = () => {
                 placeholder="请选择框架"
                 onChange={onSelectChange}
                 loading={stackLoading}
+                className={styles.inputWrap}
                 allowClear
               >
                 {
@@ -213,6 +234,23 @@ const Colony: React.FC = () => {
                   })
                 }
               </Select>
+            </Form.Item>
+            <Form.Item
+              label="kubeConfig"
+              name="kubeConfig"
+              rules={[{ required: true, message: '请输入kubeConfig!' }]}
+            >
+              {/* <Input /> */}
+              <AceEditor
+                className={styles.codeEditorWrap}
+                mode="java"
+                theme="xcode"
+                value={kubeConfig}
+                onChange={onEditorChange}
+                name="UNIQUE_ID_OF_DIV"
+                editorProps={{ $blockScrolling: true }}
+              />
+
             </Form.Item>
 
             {/* <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
