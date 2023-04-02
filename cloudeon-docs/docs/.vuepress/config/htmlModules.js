65649fa791985e981040f8ff9da51d09bf529b55
pandas886 <123344357+pandas886@users.noreply.github.com>
2023-04-02 19:51:19 +0800

    调整文档

--- cloudeon-docs/docs/.vuepress/config/htmlModules.js
+++ cloudeon-docs/docs/.vuepress/config/htmlModules.js
@@ -22,28 +22,7 @@
 module.exports = {
   // 万维广告
   pageT: `
-    <div class="wwads-cn wwads-horizontal page-wwads" data-id="136"></div>
-    <style>
-      .page-wwads{
-        width:100%!important;
-        min-height: 0;
-        margin: 0;
-      }
-      .page-wwads .wwads-img img{
-        width:80px!important;
-      }
-      .page-wwads .wwads-poweredby{
-        width: 40px;
-        position: absolute;
-        right: 25px;
-        bottom: 3px;
-      }
-      .wwads-content .wwads-text, .page-wwads .wwads-text{
-        height: 100%;
-        padding-top: 5px;
-        display: block;
-      }
-  </style>
+   
   `,
   // 赞助商广告
   sidebarT: `
