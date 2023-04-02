aef33315d7cbf8ceaec2bc75318963c45232055a
pandas886 <123344357+pandas886@users.noreply.github.com>
2023-04-02 12:24:11 +0800

    test doc ci

---
+++ cloudeon-docs/deploy.sh
@@ -0,0 +1,26 @@
+
+#!/usr/bin/env sh
+
+# 确保脚本抛出遇到的错误
+set -e
+
+
+push_addr=https://github.com/Pandas886/cloudeon-website # git提交地址，也可以手动设置，比如：push_addr=git@github.com:xugaoyi/vuepress-theme-vdoing.git
+commit_info=`git describe --all --always --long`
+dist_path=docs/.vuepress/dist # 打包生成的文件夹路径
+push_branch=master # 推送的分支
+
+# 生成静态文件
+npm run build
+
+# 进入生成的文件夹
+cd $dist_path
+
+git init
+git remote add github $push_addr
+git add -A
+git commit -m "deploy, $commit_info"
+git push -f $push_addr HEAD:$push_branch
+
+cd -
+rm -rf $dist_path
