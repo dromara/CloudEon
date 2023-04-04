fd893e7506664fd8cbdb707e574fccd455b28422
pandas886 <123344357+pandas886@users.noreply.github.com>
2023-04-04 16:17:29 +0800

    清除无用代码

--- cloudeon-server/src/main/java/com/data/udh/config/GlobalExceptionHandler.java
+++ cloudeon-server/src/main/java/com/data/udh/config/GlobalExceptionHandler.java
@@ -6,7 +6,6 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import javax.jws.WebResult;
 import javax.servlet.http.HttpServletResponse;
 
 @Slf4j
