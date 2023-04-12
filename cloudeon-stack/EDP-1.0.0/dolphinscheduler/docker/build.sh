d19d03e8193d9f74edd7719ff7624b3eae8f022e
pandas886 <123344357+Pandas886@users.noreply.github.com>
2023-04-12 14:48:06 +0800

    调整DS-修改k8s相关

--- cloudeon-stack/EDP-1.0.0/dolphinscheduler/docker/build.sh
+++ cloudeon-stack/EDP-1.0.0/dolphinscheduler/docker/build.sh
@@ -1,8 +1,8 @@
 
 
-docker build -f Dockerfile -t dolphinscheduler:3.0.4 .
-docker tag   dolphinscheduler:3.0.4  registry.cn-hangzhou.aliyuncs.com/udh/dolphinscheduler:3.0.4
-docker push registry.cn-hangzhou.aliyuncs.com/udh/dolphinscheduler:3.0.4
+docker build -f Dockerfile -t dolphinscheduler:3.0.5 .
+docker tag   dolphinscheduler:3.0.5  registry.cn-hangzhou.aliyuncs.com/udh/dolphinscheduler:3.0.5
+docker push registry.cn-hangzhou.aliyuncs.com/udh/dolphinscheduler:3.0.5
 
-docker tag   registry.cn-hangzhou.aliyuncs.com/udh/dolphinscheduler:3.0.4  registry.mufankong.top/udh/dolphinscheduler:3.0.4
-docker push  registry.mufankong.top/udh/dolphinscheduler:3.0.4
+docker tag   registry.cn-hangzhou.aliyuncs.com/udh/dolphinscheduler:3.0.5  registry.mufankong.top/udh/dolphinscheduler:3.0.5
+docker push  registry.mufankong.top/udh/dolphinscheduler:3.0.5
