25807c713be3e230826c8fb9c3b1aeb743ed376a
pandas886 <123344357+Pandas886@users.noreply.github.com>
2023-08-23 13:51:53 +0800

    初始化trino

--- cloudeon-stack/EDP-1.0.0/base/build.sh
+++ cloudeon-stack/EDP-1.0.0/base/build.sh
@@ -1,3 +1,7 @@
 docker build -f Jdk-Dockerfile -t jdk:1.8.141 .
  docker tag jdk:1.8.141  registry.cn-hangzhou.aliyuncs.com/udh/jdk:1.8.141
  docker push  registry.cn-hangzhou.aliyuncs.com/udh/jdk:1.8.141
+
+ docker build -f Jdk17-Dockerfile -t jdk:17.0.8 .
+  docker tag jdk:17.0.8  registry.cn-hangzhou.aliyuncs.com/udh/jdk:17.0.8
+  docker push  registry.cn-hangzhou.aliyuncs.com/udh/jdk:17.0.8
