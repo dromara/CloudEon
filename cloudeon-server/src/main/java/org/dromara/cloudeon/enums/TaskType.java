c9427a94dc77b06efb1ed682ba8f06857168272b
linzehao <xxx@email.com>
2024-01-14 22:01:07 +0800

    修改： 1.节点新增/删除标签任务增加重试，用于应对the object has been modified异常。 2.重构节点新增/删除标签任务，使用ApplyOrDeleteTask接口合并重复代码

--- cloudeon-server/src/main/java/org/dromara/cloudeon/enums/TaskType.java
+++ cloudeon-server/src/main/java/org/dromara/cloudeon/enums/TaskType.java
@@ -23,12 +23,12 @@ import lombok.Getter;
 @AllArgsConstructor
 public enum TaskType {
 
-    TAG_HOST(1, "添加k8s节点标签", "org.dromara.cloudeon.processor.TagHostLabelTask", true),
     START_K8S_SERVICE(2, "启动角色k8s服务", "org.dromara.cloudeon.processor.StartRoleK8sDeploymentTask", false),
+    TAG_HOST(1, "添加k8s节点标签", "org.dromara.cloudeon.processor.NodeLabelApplyTask", true),
     APPLY_ROLE_CONFIGMAP(5, "应用角色相关k8s配置", "org.dromara.cloudeon.processor.ConfigApplyTask", false),
-    CANCEL_TAG_HOST(8, "移除节点上的标签", "org.dromara.cloudeon.processor.CancelHostTagTask", true),
     STOP_K8S_SERVICE(9, "停止K8s服务", "org.dromara.cloudeon.processor.StopRoleK8sDeploymentTask", false),
     REGISTER_PROMETHEUS(11, "注册prometheus采集配置", "org.dromara.cloudeon.processor.RegisterPrometheusScrapyTask", false),
+    CANCEL_TAG_HOST(8, "移除节点上的标签", "org.dromara.cloudeon.processor.NodeLabelDeleteTask", true),
     DELETE_ROLE_CONFIGMAP(12, "删除角色相关k8s配置", "org.dromara.cloudeon.processor.ConfigDeleteTask", true),
     DELETE_SERVICE_DB_DATA(13, "删除服务相关db数据库", "org.dromara.cloudeon.processor.DeleteServiceDBDataTask", false),
     STOP_ROLE_POD(14, "停止角色Pod", "org.dromara.cloudeon.processor.StopRolePodTask", true),
